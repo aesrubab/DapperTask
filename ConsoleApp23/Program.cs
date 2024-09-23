743dafb19a9554f55c3b99e574c7ceaf4d24633c
aesrubab <rubabhuseynova013@gmail.com>
2024-09-24 01:02:27 +0400

    Add project files.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp23;
+
+
+
+var connectionString = "Data Source=WINDOWS-2P363HE;Initial Catalog=Dapper;Integrated Security=True;Connect Timeout=30;Encrypt=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+var teacherRepo = new TeacherRepository(connectionString);
+
+var newTeacher = new Teacher { Name = "Rubab", Subject = "Matematika", Email = "rb013@gmail.com", Phone = "077-777-77-77" };
+teacherRepo.Insert(newTeacher);
+
+var teacher = teacherRepo.GetById(1);
+
+var allTeachers = teacherRepo.GetAll();
+
+teacher.Name = "Rubus";
+teacherRepo.Update(teacher);
+
+teacherRepo.Delete(1);
---
+++ ConsoleApp23/TeacherRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace ConsoleApp23;
+
+public class TeacherRepository
+{
+    private readonly IDbConnection _db;
+
+    public TeacherRepository(string connectionString)
+    {
+        _db = new SqlConnection(connectionString);
+    }
+
+    public void Insert(Teacher teacher)
+    {
+        var sql = "INSERT INTO Teachers (Name, Subject, Email, Phone) VALUES (@Name, @Subject, @Email, @Phone)";
+        _db.Execute(sql, teacher);
+    }
+
+    public Teacher GetById(int id)
+    {
+        var sql = "SELECT * FROM Teachers WHERE Id = @Id";
+        return _db.Query<Teacher>(sql, new { Id = id }).FirstOrDefault();
+    }
+
+
+    public IEnumerable<Teacher> GetAll()
+    {
+        var sql = "SELECT * FROM Teachers";
+        return _db.Query<Teacher>(sql).ToList();
+    }
+
+    public void Delete(int id)
+    {
+        var sql = "DELETE FROM Teachers WHERE Id = @Id";
+        _db.Execute(sql, new { Id = id });
+    }
+
+    public void Update(Teacher teacher)
+    {
+        var sql = "UPDATE Teachers SET Name = @Name, Subject = @Subject, Email = @Email, Phone = @Phone WHERE Id = @Id";
+        _db.Execute(sql, teacher);
+    }
+}
